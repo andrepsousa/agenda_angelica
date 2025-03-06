a6db0bd7f354ab5edad6ae0c08a5b15db384548f
Andre Pereira <andre.pds07@gmail.com>
2025-03-06 16:40:37 -0300

    feat: configure Flask with SQLAlchemy and PostgreSQL

--- run.py
+++ run.py
@@ -1,6 +1,11 @@
-from app import create_app
+from app import create_app, db
 
 app = create_app()
 
+with app.app_context():
+    db.create_all()
+    print("Banco de dados criado com sucesso!")
+
 if __name__ == "__main__":
     app.run(debug=True)
+    
