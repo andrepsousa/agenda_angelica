c979c3757ab4d8f93f1b0c551b6efeb61446ccfb
Andre Pereira <andre.pds07@gmail.com>
2025-03-06 16:40:37 -0300

    feat: configure Flask with SQLAlchemy and PostgreSQL

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,25 @@
 from flask import Flask
+from flask_jwt_extended import JWTManager
+from flask_sqlalchemy import SQLAlchemy
+import os
+from datetime import timedelta
+
+db = SQLAlchemy()
+jwt = JWTManager()
+
 
 def create_app():
-    app = Flask (__name__)
+    app = Flask(__name__)
+
+    app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=10)
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY')
+
+    app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+
+    db.init_app(app)
+    jwt.init_app(app)
 
     from app.routes.routes import main
     app.register_blueprint(main)
