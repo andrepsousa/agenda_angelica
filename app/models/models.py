48603e57e8005ac518818f7dd1b10227f916a5ed
dafnye <dafny.lira@aluno.faculdadeimpacta.com.br>
2025-03-13 21:12:49 -0300

    cadastro completo de clientes

--- app/models/models.py
+++ app/models/models.py
@@ -5,9 +5,8 @@ from app import db
 class User(db.Model):
     __tablename__ = 'usuarios'
     id = db.Column(db.Integer, primary_key=True)
-    nome = db.Column(db.String(50), nullable=False)
-    endereco = db.Column(db.String(150))
-    telefone = db.Column(db.String(15))
-    cpf = db.Column(db.String(11))
+    nome = db.Column(db.String(100), nullable=False)
+    endereco = db.Column(db.String(150),nullable=False )
+    telefone = db.Column(db.String(15),nullable=False)
+    cpf = db.Column(db.String(11),unique=True, nullable=False)
     data_nascimento = db.Column(db.Date)
-    
