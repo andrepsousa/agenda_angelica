11c1515390d0b0b444b8a597e04f99056e6f5368
FrankRibe <ribeirofrank65@gmail.com>
2025-04-20 00:17:25 -0300

    Ajuste no fuso horário para América/São Paulo (GMT-3) nos contêineres Docker

--- app/routes/main_routes.py
+++ app/routes/main_routes.py
@@ -1,10 +1,36 @@
 from flask import Blueprint, render_template
+from datetime import date, datetime, timedelta
+from app.models.models import Agendamento, Service
 
 main_bp = Blueprint('main_bp', __name__, url_prefix='/inicio')
 
 @main_bp.route('/index')
 def index():
-    return render_template('index.html')  
+    hoje = date.today()
+    inicio_do_dia = datetime.combine(hoje, datetime.min.time())
+    fim_do_dia = inicio_do_dia + timedelta(days=1) - timedelta(seconds=1)
+
+    print(f"Hoje: {hoje}")
+    print(f"Início do dia: {datetime.combine(hoje, datetime.min.time())}")
+    print(f"Fim do dia: {datetime.combine(hoje, datetime.max.time())}")
+
+
+    # Busca agendamentos de hoje
+    agendamentos = Agendamento.query.filter(
+        Agendamento.data_hora >= inicio_do_dia,
+        Agendamento.data_hora <= fim_do_dia
+    ).all()
+
+    print(f"Agendamentos encontrados: {agendamentos}")  
+
+    servicos = Service.query.filter_by(status=True).all()
+
+    return render_template(
+        'index.html',
+        agendamentos=agendamentos,
+        servicos=servicos
+    )
+
 
 @main_bp.route('/sobre')
 def sobre():
